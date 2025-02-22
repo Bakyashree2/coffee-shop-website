b1729718566f199d692ced07b67375ce6b0677db
Bakyashree2 <148343190+Bakyashree2@users.noreply.github.com>
2025-02-22 10:30:17 +0530

    updated the css page

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
-}
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+};
+
 
 
 let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{ 
-    searchform.classList.toggle('active')
-    avbar.classlist.remove('active');
-    searchform.classList.remove('active');
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
     cartItem.classList.remove('active');
 }
 
@@ -19,13 +21,13 @@ let cartItem = document.querySelector('.cart-items-container');
 
 document.querySelector('#cart-btn').onclick = () =>{
     cartItem.classList.toggle('active');
-}   navbar.classlist.remove('active');
-    searchform.classList.remove('active');
-    cartItem.classList.remove('active');
+ navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}    
  
 
 window.onscroll = () =>{
-    avbar.classlist.remove('active');
-    searchform.classList.remove('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
